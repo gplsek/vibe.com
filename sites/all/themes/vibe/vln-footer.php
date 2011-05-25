adc077c2c3f8df255a914617bbd43a31a9ab2421
Joseph <jsph.niu@gmail.com>
2011-05-25 17:19:50 -0600

    Organized vln-footer.php feeds into three columns and updated css with sytling for new classes and ids.
    Signed-off-by: Joseph <jsph.niu@gmail.com>

--- sites/all/themes/vibe/vln-footer.php
+++ sites/all/themes/vibe/vln-footer.php
@@ -11,78 +11,84 @@ $feed_nodes = get_feed_nodes(array("DJ Booth Feed","All Hip Hop Feed","Datpiff F
 	<div id="vibe-lifestyle-network-header">
 		<div id="vibe-lifestyle-newtork-title">Friends of <span>VIBE</span></div>
 	</div>	
-	<div id="vibe-lifestyle-network-sites">		
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.allhiphop.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_ALLHIPHOPLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['All Hip Hop Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
+	<div id="vibe-lifestyle-network-sites">
+		<div id="vln-col-left" class="vln-col">
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.allhiphop.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_ALLHIPHOPLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['All Hip Hop Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.datpiff.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_DATPIFFLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Datpiff Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.ballerstatus.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_BALLERLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Baller Status Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>	
+		</div>
+		<div id="vln-col-middle" class="vln-col">
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+			    	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.singersroom.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_SINGERSLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Singers Room Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.popeater.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_POPEATERLOGO_SMALL.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['PopEater Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.huffingtonpost.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/huffpo_ent.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Huffington Post Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
 			</div>
 		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.datpiff.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_DATPIFFLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Datpiff Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
+		<div id="vln-col-right" class="vln-col">
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.hellobeautiful.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_HELLOLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Hello Beautiful Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></span></a></div>
+				<?php } ?>
+				</div>
 			</div>
+		 	<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.blackvoices.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_BLKVOICESLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['BlackBox Voices Feed'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>
+			<div class="vibe-lifestyle-network-site">
+				<div class="vibe-lifestyle-network-site-content_top">
+		        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.carltonjordan.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_CARLTONJORDANLOGO.jpg' border='0'></a></div>
+				<?php foreach($feed_nodes['Carlton Jordan'] as $feed_item) { ?>
+					<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
+				<?php } ?>
+				</div>
+			</div>		
 		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.ballerstatus.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_BALLERLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Baller Status Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>		
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-		    	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.singersroom.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_SINGERSLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Singers Room Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.popeater.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_POPEATERLOGO_SMALL.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['PopEater Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.huffingtonpost.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/huffpo_ent.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Huffington Post Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.hellobeautiful.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_HELLOLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Hello Beautiful Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></span></a></div>
-			<?php } ?>
-			</div>
-		</div>
-	 	<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.blackvoices.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_BLKVOICESLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['BlackBox Voices Feed'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>
-		<div class="vibe-lifestyle-network-site">
-			<div class="vibe-lifestyle-network-site-content_top">
-	        	<div class="vibe-lifestyle-network-site-image_top"><a href='http://www.carltonjordan.com/' target='_blank'><img src='/sites/all/themes/vibe/images/VIBE_FOOTER/FOOTER_CARLTONJORDANLOGO.jpg' border='0'></a></div>
-			<?php foreach($feed_nodes['Carlton Jordan'] as $feed_item) { ?>
-				<div class="vibe-lifestyle-network-item"><a target='_blank' href="<?php print $feed_item->url?>"><?php print $feed_item->title;?><span class="article_arrows">&nbsp;>></span></a></div>
-			<?php } ?>
-			</div>
-		</div>		
     </div><!-- /#vibe-lifestyle-network-sties -->
 </div>
