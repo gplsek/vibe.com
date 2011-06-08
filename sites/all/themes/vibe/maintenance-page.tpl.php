4604526a706f95489d59ac67e7784e03191509ca
Joseph <jsph.niu@gmail.com>
2011-06-07 20:42:57 -0600

    edited maintenance page to conform with site theme
    Signed-off-by: Joseph <jsph.niu@gmail.com>

--- sites/all/themes/vibe/maintenance-page.tpl.php
+++ sites/all/themes/vibe/maintenance-page.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: maintenance-page.tpl.php,v 1.1.2.1 2009/02/13 18:17:08 johnalbin Exp $
+// $Id: maintenance-page.tpl.php,v 1.2.2.1 2009/04/30 00:13:31 goba Exp $
 
 /**
  * @file maintenance-page.tpl.php
@@ -14,129 +14,85 @@
  * @see template_preprocess_maintenance_page()
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
-  <title><?php print $head_title; ?></title>
   <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
 </head>
 <body class="<?php print $body_classes; ?>">
+  <div id="page">
+    <div id="header">
+      <div id="logo-title">
 
-  <div id="page"><div id="page-inner">
+        <?php if (!empty($logo)): ?>
+          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
 
-    <a name="top" id="navigation-top"></a>
-    <?php if ($navbar): ?>
-      <div id="skip-to-nav"><a href="#navigation"><?php print t('Skip to Navigation'); ?></a></div>
-    <?php endif; ?>
-
-    <div id="header"><div id="header-inner" class="clear-block">
-
-      <?php if ($logo || $site_name || $site_slogan): ?>
-        <div id="logo-title">
-
-          <?php if ($logo): ?>
-            <div id="logo"><a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo-image" /></a></div>
+        <div id="name-and-slogan">
+          <?php if (!empty($site_name)): ?>
+            <h1 id="site-name">
+              <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
           <?php endif; ?>
 
-          <?php if ($site_name): ?>
-            <div id="site-name"><strong>
-              <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home">
-              <?php print $site_name; ?>
-              </a>
-            </strong></div>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
+          <?php if (!empty($site_slogan)): ?>
             <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
+        </div> <!-- /name-and-slogan -->
+      </div> <!-- /logo-title -->
 
-        </div> <!-- /#logo-title -->
-      <?php endif; ?>
-
-      <?php if ($header): ?>
-        <div id="header-blocks" class="region region-header">
+      <?php if (!empty($header)): ?>
+        <div id="header-region">
           <?php print $header; ?>
-        </div> <!-- /#header-blocks -->
-      <?php endif; ?>
-
-    </div></div> <!-- /#header-inner, /#header -->
-
-    <div id="main"><div id="main-inner" class="clear-block<?php if ($navbar) { print ' with-navbar'; } ?>">
-
-      <div id="content"><div id="content-inner">
-
-        <?php if ($content_top): ?>
-          <div id="content-top" class="region region-content_top">
-            <?php print $content_top; ?>
-          </div> <!-- /#content-top -->
-        <?php endif; ?>
-
-        <?php if ($title || $messages): ?>
-          <div id="content-header">
-            <?php if ($title): ?>
-              <h1 class="title"><?php print $title; ?></h1>
-            <?php endif; ?>
-            <?php if ($messages): print $messages; endif; ?>
-          </div> <!-- /#content-header -->
-        <?php endif; ?>
-
-        <div id="content-area">
-          <?php print $content; ?>
         </div>
-
-        <?php if ($content_bottom): ?>
-          <div id="content-bottom" class="region region-content_bottom">
-            <?php print $content_bottom; ?>
-          </div> <!-- /#content-bottom -->
-        <?php endif; ?>
-
-      </div></div> <!-- /#content-inner, /#content -->
-
-      <?php if ($navbar): ?>
-        <div id="navbar"><div id="navbar-inner" class="clear-block region region-navbar">
-
-          <a name="navigation" id="navigation"></a>
-          <?php print $navbar; ?>
-
-        </div></div> <!-- /#navbar-inner, /#navbar -->
       <?php endif; ?>
 
-      <?php if ($left): ?>
-        <div id="sidebar-left"><div id="sidebar-left-inner" class="region region-left">
+    </div> <!-- /header -->
+
+    <div id="container" class="clear-block">
+
+      <?php if (!empty($left)): ?>
+        <div id="sidebar-left" class="column sidebar">
           <?php print $left; ?>
-        </div></div> <!-- /#sidebar-left-inner, /#sidebar-left -->
+        </div> <!-- /sidebar-left -->
       <?php endif; ?>
 
-      <?php if ($right): ?>
-        <div id="sidebar-right"><div id="sidebar-right-inner" class="region region-right">
+      <div id="main" class="column"><div id="main-squeeze">
+
+        <div id="content">
+          <?php if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+          <?php if (!empty($messages)): print $messages; endif; ?>
+          <div id="content-content" class="clear-block">
+            <?php print $content; ?>
+          </div> <!-- /content-content -->
+        </div> <!-- /content -->
+
+      </div></div> <!-- /main-squeeze /main -->
+
+      <?php if (!empty($right)): ?>
+        <div id="sidebar-right" class="column sidebar">
           <?php print $right; ?>
-        </div></div> <!-- /#sidebar-right-inner, /#sidebar-right -->
+        </div> <!-- /sidebar-right -->
       <?php endif; ?>
 
-    </div></div> <!-- /#main-inner, /#main -->
+    </div> <!-- /container -->
 
-    <?php if ($footer || $footer_message): ?>
-      <div id="footer"><div id="footer-inner" class="region region-footer">
+    <div id="footer-wrapper">
+      <div id="footer">
+        <?php print $footer_message; ?>
+        <?php if (!empty($footer)): print $footer; endif; ?>
+      </div> <!-- /footer -->
+    </div> <!-- /footer-wrapper -->
 
-        <?php if ($footer_message): ?>
-          <div id="footer-message"><?php print $footer_message; ?></div>
-        <?php endif; ?>
-
-        <?php print $footer; ?>
-
-      </div></div> <!-- /#footer-inner, /#footer -->
-    <?php endif; ?>
-
-  </div></div> <!-- /#page-inner, /#page -->
-
-  <?php if ($closure_region): ?>
-    <div id="closure-blocks" class="region region-closure"><?php print $closure_region; ?></div>
-  <?php endif; ?>
-
-  <?php print $closure; ?>
+  </div> <!-- /page -->
 
 </body>
 </html>
