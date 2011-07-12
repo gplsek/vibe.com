717f219f83c737c98de0b69f0887b118298dd5c7
patprzybilla <patrick.przybilla@gmail.com>
2011-07-12 18:06:56 +0200

    removing related content header

--- sites/all/themes/vibe/block-related-content.tpl.php
+++ sites/all/themes/vibe/block-related-content.tpl.php
@@ -1,6 +1,6 @@
 
 
 <div id="similar_content">
-<div id="similar_content_header"><img src="<? print url('sites/all/themes/vibe/images/VIBE_RELATED_GRAPHIC.jpg'); ?>" /></div>
+<!--<div id="similar_content_header"><img src="<? print url('sites/all/themes/vibe/images/VIBE_RELATED_GRAPHIC.jpg'); ?>" /></div>-->
   <div id="vaRecWidget"></div>
 </div>
